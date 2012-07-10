74d5a39d52aef2e035affcb6f9d58f1cd65a0947
Antoine Niek <antoine.niek@bloomdigital.com>
2012-07-10 11:31:10 -0400

    quickfix for bootstrap support => https://github.com/dockyard/client_side_validations-simple_form/issues/3

--- vendor/assets/javascripts/rails.validations.simple_form.js
+++ vendor/assets/javascripts/rails.validations.simple_form.js
@@ -15,8 +15,8 @@
     add: function(element, settings, message) {
       var errorElement, wrapper;
       if (element.data('valid') !== false) {
-        wrapper = element.closest(settings.wrapper_tag);
-        wrapper.addClass(settings.wrapper_error_class);
+        wrapper = element.closest(settings.wrapper_tag + ' .controls ');
+        wrapper.parent().addClass(settings.wrapper_error_class);
         errorElement = $("<" + settings.error_tag + "/>", {
           "class": settings.error_class,
           text: message
